0cc3aca66a51ebde4123478efe4c66cc8863d175
tttcowan <tom.cowan@ymail.com>
2019-01-29 14:17:46 +0000

    Cancel coroutine tasks on clear.

--- app/src/main/java/com/appsauce/mvpappsauce/base/BackgroundTask.kt
+++ app/src/main/java/com/appsauce/mvpappsauce/base/BackgroundTask.kt
@@ -2,24 +2,30 @@ package com.appsauce.mvpappsauce.base
 
 import com.appsauce.mvpappsauce.extension.logE
 import com.appsauce.mvpappsauce.extension.tag
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class BackgroundTask {
 
     private var clear: Boolean = false
+    private val tasks: MutableList<CoroutineScope> = mutableListOf()
 
     fun <T> run(task: () -> T, complete: (value: T) -> Unit, error: (e: Exception) -> Unit) {
         clear = false
         GlobalScope.launch {
+            tasks.add(this)
             try {
                 if (!clear) {
                     val value = task()
+                    tasks.remove(this)
                     withContext(Dispatchers.Main) { complete(value) }
                 }
             } catch (e: Exception) {
+                tasks.remove(this)
                 "Call failed".logE(tag(), e)
                 if (!clear) {
                     withContext(Dispatchers.Main) { error(e) }
@@ -29,6 +35,15 @@ class BackgroundTask {
     }
 
     fun clear() {
+        tasks.forEach {
+            try {
+                it.cancel()
+            }
+            catch (e : IllegalStateException) {
+                //Task already cancelled.
+            }
+        }
+        tasks.clear()
         clear = true
     }
 }
